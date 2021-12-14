20fcb4ee1cc15ef110414d31afdd27e91d823b15
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2021-12-14 19:59:49 +0300

    Added try catch to the get action

--- src/actions/tournaments.ts
+++ src/actions/tournaments.ts
@@ -1,27 +1,30 @@
 import { API_TOURNAMENTS_URL } from './../constants/api';
 import { actionTypes } from './constants';
-import { RootState } from '../reducers';
 
 // getTournaments from api and dispatch action to store
-export const getTournaments = () => async (
-  dispatch: any,
-  getState: () => RootState
-) => {
-  dispatch({
-    type: actionTypes.SET_LOADING,
-    payload: true
-  });
+export const getTournaments = () => async (dispatch: any) => {
+  try {
+    dispatch({
+      type: actionTypes.SET_LOADING,
+      payload: true
+    });
 
-  const response = await fetch(API_TOURNAMENTS_URL);
-  const tournaments = await response.json();
+    const response = await fetch(API_TOURNAMENTS_URL);
+    const tournaments = await response.json();
 
-  dispatch({
-    type: actionTypes.GET_TOURNAMENTS,
-    payload: tournaments
-  });
-
-  dispatch({
-    type: actionTypes.SET_LOADING,
-    payload: false
-  });
+    dispatch({
+      type: actionTypes.GET_TOURNAMENTS,
+      payload: tournaments
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_TOURNAMENTS,
+      payload: []
+    });
+  } finally {
+    dispatch({
+      type: actionTypes.SET_LOADING,
+      payload: false
+    });
+  }
 };
